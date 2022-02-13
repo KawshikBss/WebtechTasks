e6138a41941d26b2273cbf78780bf8c51a0f84b6
KawshikBss <kawshikbss@gmail.com>
2022-02-13 11:56:10 +0600

    created and completed task2

---
+++ MID_LAB_TASK_05_FORM_SUBMIT/task2.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>EMAIL</title>
+</head>
+<body>
+    <?php
+        $email = '';
+        if ($_REQUEST)
+        {
+            if (isset($_REQUEST["email"]))
+                $email = $_REQUEST["email"];
+        }
+    ?>
+    <form action="task2_handler.php" method="post">
+        <fieldset style="width: 20%;">
+            <legend>EMAIL</legend>
+            <input type="text" name="email" value=<?php echo $email ?>>
+            <hr>
+            <input type="submit" value="Submit">
+        </fieldset>
+    </form>
+    <h2><?php echo $email ?></h2>
+</body>
+</html>
